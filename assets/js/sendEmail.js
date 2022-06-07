6af65938b4c048645ba477321d1cebfd07423d13
van-essa <vanessa.andersson3@gmail.com>
2022-06-07 15:51:23 +0000

    add email integrasion on the contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,16 @@
+function sendEmail(contactForm) {
+    emailjs.send("service_dqks2kp","Rosie", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
